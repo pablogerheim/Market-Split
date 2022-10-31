081d3a04bbf9428d5ed4b5015ac6794c47833a5e
Pablo <pablogerheim@gmail.com>
2022-10-31 19:32:28 -0300

    vercel test 4

--- frontreact/src/Router.tsx
+++ frontreact/src/Router.tsx
@@ -14,6 +14,7 @@ import { Login } from './pages/Login'
 import { useContext } from 'react';
 import { AuthContext } from './contexts/AuthContext';
 import { CreateGroup } from "./pages/group/CreateGroup";
+import React from 'react';
 
 function Router() {
  const auth = useContext(AuthContext);
