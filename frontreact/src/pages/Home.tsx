953796e0865c781e39a2d14ac89ea2a36206b589
Pablo <pablogerheim@gmail.com>
2022-11-10 20:36:08 -0300

    logout 4

--- frontreact/src/pages/Home.tsx
+++ frontreact/src/pages/Home.tsx
@@ -16,32 +16,33 @@ function Home() {
   const [close, setClose] = useState(true)
   const [erro, setErro] = useState<string>()
   const [user] = useState(auth.user)
+  const [opacity, setOpacity] = useState(false)
 
   const logoutApp = async () => {
-     auth.signout();
-   // window.location.href = "https://market-split.vercel.app/"
+    auth.signout();
+    setOpacity(true)
   };
 
   useEffect(() => {
     !user?.group_member && setErro(" User missing please reload")
-      if (user) {
-       const getPurchases = async () => {
-        const data = await api.getPurchase(user?.group_member).catch(onrejected => 
+    if (user) {
+      const getPurchases = async () => {
+        const data = await api.getPurchase(user?.group_member).catch(onrejected =>
           console.log("descrição do erro", onrejected))
         setPurchases(data?.data)
       }
-      getPurchases() 
+      getPurchases()
     } else setErro("session expired")
   }, [])
 
-  const handlepurchase = async (id: number, name:string) => {
-   auth.getPurchasebyid({purchaseId: id, name})
-   navegat('/session')
+  const handlepurchase = async (id: number, name: string) => {
+    auth.getPurchasebyid({ purchaseId: id, name })
+    navegat('/session')
   }
 
   return (<>
     {close || <NewPurchaseDialog setClose={setClose} />}
-    <div className={`p-5 bg-white mt-1 w-[90%] h-[87vh] border-8 ${close || 'opacity-20'}`} >
+    <div className={`p-5 bg-white mt-1 w-[90%] h-[87vh] border-8 ${close || 'opacity-20'} ${opacity && 'opacity-40'}`} >
       <button
         onClick={() => setClose(false)}
         className='start "border-gray-300 border-solid border-b-4 bg-sky-300 px-10 py-4 rounded-md mt-5 text-3xl'
@@ -57,8 +58,8 @@ function Home() {
         </button>
         <p className="text-red-500 mt-2">{erro && erro} </p>
         <div className='overflow-board h-[66%] mt-4 min-w-[300px]'>
-        {!purchases && <p className='mt-5'> Loading...</p>}
-          {purchases?.map(p => <button key={p.purchaseId} onClick={() => handlepurchase(p.purchaseId,p.name)}
+          {!purchases && <p className='mt-5'> Loading...</p>}
+          {purchases?.map(p => <button key={p.purchaseId} onClick={() => handlepurchase(p.purchaseId, p.name)}
             className='start flex flex-col w-[275px] max-w-[275px] mx-4 "border-gray-300 border-solid border-b-4 bg-indigo-300 px-16 py-2 rounded-md mt-5 text-3xl'>
             <h2>{p.name}</h2>
             <p className='text-xl'>{p.timestamp}</p>
